c22e0b8d8958114a7dc57fdb73fc8add8281daa4
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2017-03-16 19:35:24 -0400

    main logic

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
 #!/usr/bin/env node
 
-console.log("Work in progress!");
+const argv = (function(optimist) {
+	var cmd = optimist.usage('Usage: $0 --host hostname --adoptionfolder /etc/unifi-adoption-bot');
+
+	var required = ['host', 'adoptionfolder'];
+
+	if(!process.env['UNIFI_USER']) {
+		required.push('user');
+	}
+
+	if(!process.env['UNIFI_PASS']) {
+		required.push('pass');
+	}
+
+	cmd.describe('logfile', 'Path to unifi server\'s log file')
+	   .default('logfile', '/var/log/unifi/server.log')
+	   .describe('user', 'Unifi\'s username (default to env.UNIFI_USER)')
+	   .describe('pass', 'Unifi\'s password (default to env.UNIFI_PASS)')
+	   .describe('host', 'Unifi\'s hostname')
+	   .describe('port', 'Unifi\'s port')
+	   .default('port', 8443)
+	   .describe('adoptionfolder', 'Folder containing lists of AP')
+	   .demand(required);
+
+	var argv = cmd.argv;
+
+	if(!argv.user && process.env['UNIFI_USER']) {
+		argv.user = process.env['UNIFI_USER'];
+	}
+
+	if(!argv.pass && process.env['UNIFI_PASS']) {
+		argv.pass = process.env['UNIFI_PASS'];
+	}
+
+    return argv;
+})(require('optimist'));
+
+const unifi = require('./unifi')(argv.host, argv.port);
+
+
+unifi.login(argv.user, argv.pass).then(function() {
+	console.log("Connected to unifi controler");
+}, function(err) {
+	console.error("Couldn't connect to unifi's controler.", err);
+	process.exit(1);
+});
+
+
+const adoptingReg = /\[([^\]]+)\] <([^>]+)>.+AP\[([a-fA-F0-9:]+)\] was discovered and waiting for adoption/;
+
+const conf = require('./adopt-conf.js')(argv.adoptionfolder);
+
+const watcher = require('./watcher')(argv.logfile, function(line) {
+	var res = adoptingReg.exec(line);
+
+	if(res) {
+		var mac = res[3];
+		conf.findAp(mac).then((aps) => {
+			if(aps.length === 0) {
+				console.log("AP[%s] waiting for adoption not found in config file", mac);
+			} else if(aps.length > 1) {
+				console.log("Found AP[%s] in multiple config files (%s)", mac, aps.map((ap) => ap.site).join(', '));
+			} else {
+				var ap = aps[0];
+				console.log("Adopting AP[%s] to %s", mac, ap.site);
+				return unifi.adopt(mac, ap.site);
+			}
+		}).then(() => {
+			console.log("Successfuly adopted AP[%s]", mac);
+		}, (err) => {
+			console.log("Error adopting AP[%s]:", mac, err);
+		});
+	}
+});
